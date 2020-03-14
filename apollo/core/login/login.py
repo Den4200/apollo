ff0a05efbb35ea30584fbf30bdcbb0485c98ef8f
Den4200 <dpham.42@hotmail.com>
2020-03-14 18:13:15 -0400

    popups for invalid logins & duplicate username registrations

--- apollo/core/login/login.py
+++ apollo/core/login/login.py
@@ -1,8 +1,11 @@
 from functools import partial
 
-from kivy.lang import Builder
 from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
+
+from frost.client import Status
 
 from apollo.core.listener import listen
 from apollo.core.client import client, messages
@@ -13,22 +16,32 @@ Builder.load_file('apollo/core/login/login.kv')
 class LoginScreen(Screen):
 
     def login(self, username, password):
-        client.login(username, password)
+        status = client.login(username, password)
 
-        # Display previous chat messages
-        msgs = client.get_all_msgs()
-        updater = self.manager.messages_screen.messages.message_history.update_chat_history
-        for msg_id, msg in msgs.items():
-            messages.contents[msg_id] = msg
+        if status == Status.SUCCESS.value:
+            # Display previous chat messages
+            msgs = client.get_all_msgs()
+            updater = self.manager.messages_screen.messages.message_history.update_chat_history
+            for msg_id, msg in msgs.items():
+                messages.contents[msg_id] = msg
 
-            updater(
-                f'[color=dd2020]{msg["from_user"]["username"]}[/color] > {msg["message"]}'
-            )
+                updater(
+                    f'[color=dd2020]{msg["from_user"]["username"]}[/color] > {msg["message"]}'
+                )
 
-        Clock.schedule_interval(partial(listen, updater), 0.25)
+            Clock.schedule_interval(partial(listen, updater), 0.25)
 
-        self.manager.current = 'messages_screen'
+            self.manager.current = 'messages_screen'
+
+        else:
+            LoginFailure().open()
+
+        self.reset_form()
 
     def reset_form(self):
         self.ids['username'].text = ''
         self.ids['password'].text = ''
+
+
+class LoginFailure(Popup):
+    pass
--- apollo/core/register/register.py
+++ apollo/core/register/register.py
@@ -1,5 +1,8 @@
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
+
+from frost.client import Status
 
 from apollo.core.client import client
 
@@ -10,5 +13,20 @@ Builder.load_file('apollo/core/register/register.kv')
 class RegisterScreen(Screen):
 
     def register(self, username, password):
-        client.register(username, password)
-        self.manager.current = 'login_screen'
+        status = client.register(username, password)
+
+        if status == Status.SUCCESS.value:
+            self.manager.current = 'login_screen'
+
+        else:
+            RegistrationFailure().open()
+
+        self.reset_form()
+
+    def reset_form(self):
+        self.ids['username'].text = ''
+        self.ids['password'].text = ''
+
+
+class RegistrationFailure(Popup):
+    pass
